7c06822f0b4b3e032e342a87b7c7afe6cf62e641
Chase Qi <chase.qi@linaro.org>
2014-09-29 10:58:15 +0800

    add lava-test-case

---
+++ android/scripts/lava-tset-case.sh
@@ -0,0 +1,28 @@
+#!/system/bin/sh
+#
+# toolchain test cases for Linaro Android
+#
+# Copyright (C) 2013, Linaro Limited.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# Author: Chase Qi <chase.qi@linaro.org>
+
+# Test case definitions
+lava-test-case lava-test-case --result pass --measurement 9999 --units bottlessss
+
+
+# clean exit so lava-test can trust the results
+exit 0
