d575e7b3ad33f417ab8ada10a51cf7a10366660c
Chase Qi <chase.qi@linaro.org>
2014-10-10 11:19:50 +0800

    update toolchain measurement

--- android/scripts/toolchain-android.sh
+++ android/scripts/toolchain-android.sh
@@ -20,23 +20,57 @@
 #
 # Author: Chase Qi <chase.qi@linaro.org>
 
-# Test case definitions
-toolchain(){
+## Test case definitions
+# Check if toolchain version not empty
+toolchain-not-empty() {
+    echo -e "\n================"
+    echo "Check if toolchain version not empty"
     echo "Content of /proc/version:"
     echo `cat /proc/version`
-    gcc=`grep "Linaro GCC" /proc/version`
-    if [ -z "$gcc" ]
+    version=`grep "Linaro GCC" /proc/version`
+    if [ -z "$version" ]
     then
-        echo "toolchain:" "fail" "gcc not exist"
+        echo "toolchain-not-empty:" "fail" "0"
         return 1
     else
-        measurement=`awk '{print substr($5,2,4),$6,$7,$8,$9,$10,$11,$12;}' /proc/version`
-        echo "toolchain:" "pass" "$measurement"
+        echo "toolchain-not-empty:" "pass" "0"
+    fi
+}
+
+# Print GCC version in result bundle
+gcc-version() {
+    echo -e "\n================"
+    echo "Print GCC version"
+    GCC=`awk '{print $7;}' /proc/version`
+    ReleaseDate=`awk '{print $8;}' /proc/version`
+    GCCVersion=$GCC-$ReleaseDate
+    if [ -z "$GCCVersion" ]
+    then
+        echo "gcc-version:" "fail" "0"
+        return 1
+    else
+        echo "gcc-version:" "pass" "$GCCVersion"
+    fi
+}
+
+# Print Linaro GCC version in result bundle
+linaro-gcc-version() {
+    echo -e "\n================"
+    echo "Print Linaro GCC version"
+    LinaroGCC=`awk '{print substr($12,1,11)}' /proc/version`
+    if [ -z "$LinaroGCC" ]
+    then
+        echo "linaro-gcc-version:" "fail" "0"
+        return 1
+    else
+        echo "linaro-gcc-version:" "pass" "$LinaroGCC"
     fi
 }
 
 # run the test
-toolchain
+toolchain-not-empty
+gcc-version
+linaro-gcc-version
 
 # clean exit so lava-test can trust the results
 exit 0
