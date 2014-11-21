968fe7e66c141efda7dbc09bfc70530b5d0d1b1e
Chase Qi <chase.qi@linaro.org>
2014-11-21 10:26:38 +0800

    pi_stress major update

--- ubuntu/scripts/pi-stress-test.sh
+++ ubuntu/scripts/pi-stress-test.sh
@@ -1,24 +1,21 @@
-#!/bin/bash 
+#!/bin/sh 
 
 DURATION=$1
 
-# ignore the SIGTERM sign from pi_stress when test failed.
+# ignore the terminated signal from pi_stress when test failed.
 trap '' SIGTERM
 
-pi_stress --mlockall --duration $DURATION  > pi_stress_test.log 2>&1 
+# run pi_stress test with customized running time
+pi_stress --mlockall --duration $DURATION  > pi-stress-test.log 2>&1 
 
 if [ $? -eq 0 ]; then
-    lava-test-case pi-stress-test --result pass --measurement=0 --units=groups
+    grep "Total inversion performed" pi-stress-test.log
+    lava-test-case pi-stress-test --result pass
 else
-    if [ -n "`grep deadlocked pi_stress_test.log`" ]; then
-        grep "ERROR\|deadlocked" pi_stress_test.log
-        num_groups=`grep deadlocked pi_stress_test.log | wc -l`
-        lava-test-case pi-stress-test --result fail --measurement=$num_groups --units=groups
-    else
-        grep ERROR pi_stress_test.log
-        lava-test-case pi-stress-test --result fail
-    fi
+    grep ERROR pi-stress-test.log
+    lava-test-case pi-stress-test --result fail
 fi
 
-lava-test-run-attach pi_stress_test.log text/plain
+# attach the pi-stress-test.log file to pi-stress-test
+lava-test-run-attach pi-stress-test.log text/plain
 
