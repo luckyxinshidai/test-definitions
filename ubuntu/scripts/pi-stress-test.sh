749259016f40b5593e0e12c42893c22d2b475ea7
Chase Qi <chase.qi@linaro.org>
2014-11-25 10:09:09 +0800

    major update

--- ubuntu/scripts/pi-stress-test.sh
+++ ubuntu/scripts/pi-stress-test.sh
@@ -1,21 +1,40 @@
 #!/bin/sh 
 
 DURATION=$1
+GROUP=$2
+MLOCKALL=$3
+RR=$4
+LogFile="pistress.log"
+
+OPTIONS="--duration $DURATION"
+
+if [ "$GROUP" != "default" ]; then
+    OPTIONS="$OPTIONS --groups $GROUP"
+fi
+
+if [ "$MLOCKALL" == "true" ]; then 
+    OPTIONS="$OPTIONS --mlockall"
+fi
+
+if [ "$RR" != "false" ]; then
+    OPTIONS="$OPTIONS --rr"
+fi
+
+echo "Running pi_stress with options: $OPTIONS"
 
 # ignore the terminated signal from pi_stress when test failed.
 trap '' SIGTERM
 
-# run pi_stress test with customized running time
-LogFile="pi_stress.log"
-pi_stress --mlockall --duration $DURATION  > $LogFile 2>&1 
+# run pi_stress test
+echo "========"
+echo "Running pi_stress with options: $OPTIONS"
+pi_stress $OPTIONS  > $LogFile 2>&1 
 
 if [ $? -eq 0 ]; then
     grep "Total inversion performed" $LogFile
-    lava-test-run-attach $LogFile
     lava-test-case pi-stress-test --result pass
 else
     grep ERROR $LogFile
-    lava-test-run-attach $LogFile
     lava-test-case pi-stress-test --result fail
 fi
 
