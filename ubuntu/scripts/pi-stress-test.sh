55cec2a803834d99281af3d57ed31602bb51b38f
Chase Qi <chase.qi@linaro.org>
2014-11-25 13:07:42 +0800

    majore update

--- ubuntu/scripts/pi-stress-test.sh
+++ ubuntu/scripts/pi-stress-test.sh
@@ -1,18 +1,38 @@
-#!/bin/sh 
+#!/bin/bash
+#
+# pi_stress test cases for Linux Linaro ubuntu
+#
+# Copyright (C) 2012 - 2014, Linaro Limited.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# Author: Chase Qi <chase.qi@linaro.org>
+#
 
 DURATION=$1
 GROUP=$2
 MLOCKALL=$3
 RR=$4
-LogFile="pi_stress.log"
-
 OPTIONS="--duration $DURATION"
+LogFile="pi_stress.log"
 
 if [ "$GROUP" != "default" ]; then
     OPTIONS="$OPTIONS --groups $GROUP"
 fi
 
-if [ "$MLOCKALL" = "true" ]; then 
+if [ "$MLOCKALL" = "true" ]; then
     OPTIONS="$OPTIONS --mlockall"
 fi
 
@@ -20,20 +40,17 @@ if [ "$RR" != "false" ]; then
     OPTIONS="$OPTIONS --rr"
 fi
 
-# ignore the terminated signal from pi_stress when test failed.
-trap '' SIGTERM > /dev/null 2>&1
-
-# run pi_stress test
 echo "========"
-echo "Running pi_stress with options: $OPTIONS"
-pi_stress $OPTIONS  > $LogFile 2>&1 
+echo "Running pi_stress test with options: $OPTIONS"
+trap '' SIGTERM
+pi_stress $OPTIONS > $LogFile 2>&1
 
 if [ $? -eq 0 ]; then
     echo "pi_stress test finished successfully"
     grep "Total inversion performed" $LogFile
     lava-test-case pi-stress-test --result pass
 else
+    echo "Error occurred, pi_stresss test failed"
     grep ERROR $LogFile
     lava-test-case pi-stress-test --result fail
 fi
-
