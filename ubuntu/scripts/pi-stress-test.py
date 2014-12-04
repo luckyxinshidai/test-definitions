d7e5a3554920956f4ef02b1a6e0fa62d19b19ffb
Chase Qi <chase.qi@linaro.org>
2014-12-04 11:58:55 +0800

    minor update

--- ubuntu/scripts/pi-stress-test.py
+++ ubuntu/scripts/pi-stress-test.py
@@ -45,7 +45,7 @@ signal.signal(signal.SIGTERM, signal.SIG_IGN)
 
 # Run PI stress test
 print 'pi_stress_command is:'
-print " ".join(pi_stress_command)
+print ' '.join(pi_stress_command)
 if call(pi_stress_command) == 0:
     call(['lava-test-case', 'pi-stress-test', '--result', 'pass'])
 else:
