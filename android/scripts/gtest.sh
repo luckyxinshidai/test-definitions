ac41cb4b06a21e97844ff368599f8a44dda9eff7
Chase Qi <chase.qi@linaro.org>
2015-01-21 16:16:18 +0800

    android: add gest.yaml

--- android/scripts/gtest.sh
+++ android/scripts/gtest.sh
@@ -5,28 +5,47 @@ set -x
 TESTS=$1
 
 for i in $TESTS; do
+    # Detect file attribute.
     DIR="/data/nativetest"
-
-    # Detect file attribute
     if [ -e $DIR/$i ]; then
         test -d $DIR/$i && DIR="$DIR/$i"
         chmod 755 $DIR/$i
     else
         echo "$i test NOT found."
-        echo "lava-test-case $i --result fail"
+        lava-test-case $i --result fail
         continue
     fi
 
-    # Run test 
+    # Run test.
     chmod 755 $DIR/$i
-    $DIR/$i --gtest_output="xml:$i.xml"
-    grep "<testsuite name=" $i.xml | awk -F '"' '{print $2,$4,$6}' > $i.xml.parsed
+    # Nonzero exit code will terminate test script, use "||true" as work around.
+    $DIR/$i --gtest_output="xml:$i.xml" || true
+    if [ -f $i.xml ]; then
+        echo "Generated XML report successfullu."
+    else
+        echo "XML report NOT found."
+        lava-test-case $i --result fail
+        continue
+    fi
+
+    # Parse test result.
+    grep "<testsuite name=" $i.xml | awk -F '"' '{print $2,$6}' > $i.xml.parsed
+    if [ $? -ne 0 ]; then
+        echo "Valid test result NOT found"
+        lava-test-case $i --result fail
+        continue
+    fi
+
     while read line; do
-        array=($line)
-        if [ ${array[2]} -eq 0 ]; then
-            echo "lava-test-case ${array[0]} --result pass"
-        else
-            echo "lava-test-case ${array[0]} --result fail"
-        fi
+            TestCaseID=`echo $line | awk '{print $1}'`
+            echo $TestCaseID
+            Failures=`echo $line | awk '{print $2}'`
+            echo $Failures
+
+            if [ $Failures -eq 0 ]; then
+                lava-test-case $TestCaseID --result pass
+            else
+                lava-test-case $TestCaseID --result fail
+            fi
     done < $i.xml.parsed
 done
