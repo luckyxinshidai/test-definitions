d19781120f333e19514fafe66466981389e93b09
Chase Qi <chase.qi@linaro.org>
2015-02-04 11:32:25 +0800

    android: update gtest.sh

--- android/scripts/gtest.sh
+++ android/scripts/gtest.sh
@@ -1,6 +1,26 @@
 #!/system/bin/sh
-set -e
-set -x
+#
+# gtest test case for Linux Linaro Android
+#
+# Copyright (C) 2012 - 2014, Linaro Limited.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# Author: Milosz Wasilewski <milosz.wasilewski@linaro.org>
+#         Chase Qi <chase.qi@linaro.org>
+#
 
 TESTS=$1
 ScriptDIR=`pwd`
@@ -9,7 +29,7 @@ FilesDIR="/data/data/org.linaro.gparser/files"
 # Download and install gparser.apk
 wget http://people.linaro.org/~chase.qi/apks/gparser.apk
 chmod -R 777 $ScriptDIR
-pm install "$ScriptDIR/gparser.apk" || echo "gparser.apk installation failed"
+pm install "$ScriptDIR/gparser.apk"
 mkdir $FilesDIR
 
 for i in $TESTS; do
@@ -30,9 +50,14 @@ for i in $TESTS; do
     # Parse test result.
     cp $ScriptDIR/$TestCaseName.xml $FilesDIR/TestResults.xml
     chmod -R 777 $FilesDIR
-    am start -n org.linaro.gparser/.MainActivity || echo "Failed to start MainActivity"
+
+    # Start gparser MainActivity, TestResults.xml will be parsed automatically.
+    # Parsed result will be saved as ParsedTestResults.txt under the same directory.
+    am start -n org.linaro.gparser/.MainActivity
     sleep 15
-    am force-stop org.linaro.gparser || echo "Failed to stop gparser"
+    # Stop gparser for the next loop.
+    am force-stop org.linaro.gparser
+
     if [ -f $FilesDIR/ParsedTestResults.txt ]; then
         echo "XML report parsed successfully."
         mv $FilesDIR/ParsedTestResults.txt $ScriptDIR/$TestCaseName.ParsedTestResults.txt
@@ -42,12 +67,12 @@ for i in $TESTS; do
         continue
     fi
 
-    # Collect test results 
+    # Collect test results.
     while read line; do
             TestCaseID=`echo $line | awk '{print $1}'`
             TestResult=`echo $line | awk '{print $2}'`
-            
-            # Use test case name as prefix.
+
+            # Use test case name as prefix to amend TestCaseID.
             lava-test-case $TestCaseName.$TestCaseID --result $TestResult
     done < $ScriptDIR/$TestCaseName.ParsedTestResults.txt
 done
