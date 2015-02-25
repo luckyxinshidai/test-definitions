b45aa1d84e65d5cbe9d88426cc3ffac9fd49f1fb
Chase Qi <chase.qi@linaro.org>
2015-02-25 21:52:38 +0800

    ubuntu: update mmtests

--- common/scripts/mmtests.sh
+++ common/scripts/mmtests.sh
@@ -9,16 +9,21 @@ DIR=`pwd`
 # Download tests directly, rather than use the customized mirror.
 sed -i '/WEBROOT/s/^/#/' $DIR/shellpacks/common-config.sh
 
-# Result parse
+# Result parsing
 result_parse(){
     local TEST_ID=$1
     case $TEST_ID in
         dd|dd-tmpfs|ddsync)
-            if [ -e $DIR/work/log/loopdd-$KernelVersion/noprofile/dd.30 ]; then
-                ddloop_speed=`grep copied $DIR/work/log/loopdd-$KernelVersion/noprofile/dd.30 |grep copied|awk '{print $8}'`
-                lava-test-case $TEST_ID --result pass --measurement $ddloop_speed --units MB/s
-            else
+            if [ -z `grep copied $DIR/work/log/loopdd-$KernelVersion/noprofile/mmtests.log` ]; then
                 lava-test-case $TEST_ID --result fail
+            else
+                units=`grep copied $DIR/work/log/loopdd-$KernelVersion/noprofile/dd.30 | awk '{print $9}'`
+                # Get the min, max and mean scores of the 30 iterations
+                grep copied $DIR/work/log/loopdd-$KernelVersion/noprofile/mmtests.log | awk '{print $8}' > $DIR/$TEST_ID-data.txt
+                eval `awk '{if(min=="") {min=max=$1}; if($1>max) {max=$1}; if($1< min) {min=$1}; total+=$1; count+=1} END {print "mean="total/count, "min="min, "max="max}' $DIR/$TEST_ID-data.txt`
+                lava-test-case $TEST_ID-min --result pass --measurement $min --units $units
+                lava-test-case $TEST_ID-max --result pass --measurement $max --units $units
+                lava-test-case $TEST_ID-mean --result pass --measurement $mean --units $units
             fi
             ;;
     esac
@@ -26,7 +31,7 @@ result_parse(){
 
 ## Run tests
 for SUB_TEST in $TESTS; do
-    $DIR/run-mmtests.sh --config configs/config-global-dhp__$SUB_TEST $KernelVersion
+    $DIR/run-mmtests.sh --no-monitor --config configs/config-global-dhp__$SUB_TEST $KernelVersion
     if [ $? -ne 0 ]; then
         lava-test-case $SUB_TEST --result fail
     else
