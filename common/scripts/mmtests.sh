8c1c64aeb8a082223066f722bc74c47b123668e5
Chase Qi <chase.qi@linaro.org>
2015-02-26 21:16:14 +0800

    ubuntu: add mmtests-libmicro

--- common/scripts/mmtests.sh
+++ common/scripts/mmtests.sh
@@ -9,7 +9,7 @@ DIR=`pwd`
 # Download tests directly, rather than use the customized mirror.
 sed -i '/WEBROOT/s/^/#/' $DIR/shellpacks/common-config.sh
 
-# Result parsing
+# Result parser
 result_parse(){
     local TEST_ID=$1
     case $TEST_ID in
@@ -17,7 +17,7 @@ result_parse(){
             if [ -z `grep copied $DIR/work/log/loopdd-$KernelVersion/noprofile/mmtests.log` ]; then
                 lava-test-case $TEST_ID --result fail
             else
-                units=`grep copied $DIR/work/log/loopdd-$KernelVersion/noprofile/dd.30 | awk '{print $9}'`
+                units=`grep copied $DIR/work/log/loopdd-$KernelVersion/noprofile/mmtests.log | tail -1 | awk '{print $9}'`
                 # Get the min, max and mean scores of the 30 iterations
                 grep copied $DIR/work/log/loopdd-$KernelVersion/noprofile/mmtests.log | awk '{print $8}' > $DIR/$TEST_ID-data.txt
                 eval `awk '{if(min=="") {min=max=$1}; if($1>max) {max=$1}; if($1< min) {min=$1}; total+=$1; count+=1} \
@@ -32,13 +32,25 @@ result_parse(){
                 lava-test-case $TEST_ID --result fail
             else
                # Use the final total average value as measurement
-               TotalAverage=`grep "Total Average.*us" $DIR/work/log/ku_latency-$KernelVersion/noprofile/ku-latency.log \
+               ku_total_average=`grep "Total Average.*us" $DIR/work/log/ku_latency-$KernelVersion/noprofile/ku-latency.log \
                              | tail -1 | awk '{print $6}'`
                # Use the final rolling average value as measurement
-               RollingAverage=`grep "Rolling Average.*us" $DIR/work/log/ku_latency-$KernelVersion/noprofile/ku-latency.log \
+               ku_rolling_average=`grep "Rolling Average.*us" $DIR/work/log/ku_latency-$KernelVersion/noprofile/ku-latency.log \
                                | tail -1 | awk '{print $6}'`
-               lava-test-case $TEST_ID-total-average --result pass --measurement $TotalAverage --units us
-               lava-test-case $TEST_ID-rolling-average --result pass --measurement $RollingAverage --units us
+               lava-test-case $TEST_ID-total-average --result pass --measurement $ku_total_average --units us
+               lava-test-case $TEST_ID-rolling-average --result pass --measurement $ku_rolling_average --units us
+            fi
+            ;;
+        libmicro)
+            if [ -z `grep Running $DIR/work/log/libmicro-$KernelVersion/noprofile/mmtests.log` ]; then
+                lava-test-case $TEST_ID --result fail
+            else
+                for i in `ls $DIR/work/log/libmicro-$KernelVersion/noprofile/memset*`; do
+                    # Get test name and usecs/call value in the 3th line of each test log.
+                    libmicro_test_name=`sed -n 3p $i | awk '{print $1}'`
+                    libmicro_test_result=`sed -n 3p $i | awk '{print $4}'`
+                    lava-test-case $TEST_ID-$libmicro_test_name-median --result pass --measurement $libmicro_test_result --units usecs/call
+                done
             fi
             ;;
     esac
