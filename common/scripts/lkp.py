d0835ab19c2f65f805f632d1e484241af6734fab
Chase Qi <chase.qi@linaro.org>
2015-07-17 11:59:28 +0800

    ubuntu: reform lkp

--- common/scripts/lkp.py
+++ common/scripts/lkp.py
@@ -30,33 +30,34 @@ import pwd
 import shutil
 from subprocess import call
 
-LKPPath = str(sys.argv[1])
-print 'LKP test suite path: %s' % (LKPPath)
+LKP_PATH = str(sys.argv[1])
+print 'LKP test suite path: %s' % (LKP_PATH)
 WD = str(sys.argv[2])
 print 'Working directory: %s' % (WD)
 LOOPS = int(sys.argv[3])
 JOB = str(sys.argv[4])
+MONITORS = str(sys.argv[5])
 print 'Going to run %s %s times' % (JOB, LOOPS)
-HostName = platform.node()
-KernelVersion = platform.release()
+HOST_NAME = platform.node()
+KERNEL_VERSION = platform.release()
 DIST = str.lower(platform.dist()[0])
 CONFIG = 'defconfig'
 
 
-def test_result(TestCommand, TestCaseID):
+def test_result(TEST_COMMAND, TEST_CASE_ID):
     # For each step of test run, print pass or fail to test log.
-    if call(TestCommand) == 0:
-        print '%s pass' % (TestCaseID)
+    if call(TEST_COMMAND) == 0:
+        print '%s pass' % (TEST_CASE_ID)
         return True
     else:
-        print '%s fail' % (TestCaseID)
+        print '%s fail' % (TEST_CASE_ID)
         return False
 
 
-def find_user(name):
+def find_user(NAME):
     # Create user 'lkp' if it doesn't exist.
     try:
-        return pwd.getpwnam(name)
+        return pwd.getpwnam(NAME)
     except KeyError:
         return None
 
@@ -73,26 +74,25 @@ if not os.path.exists('/home/lkp'):
 
 call(['chown', '-R', 'lkp:lkp', '/home/lkp'])
 
-# f = open('/etc/apt/sources.list.d/multiverse.list', 'w')
-# f.write('deb http://ports.ubuntu.com/ubuntu-ports/ vivid multiverse\n')
-# f.close()
+f = open('/etc/apt/sources.list.d/multiverse.list', 'w')
+f.write('deb http://ports.ubuntu.com/ubuntu-ports/ vivid multiverse\n')
+f.close()
 call(['apt-get', 'update'])
+
+# Setup test job.
+SETUP_JOB = [LKP_PATH + '/bin/lkp install',
+             LKP_PATH + '/jobs/' + JOB + '.yaml']
+print 'Set up %s test with command: %s' % (JOB, SETUP_JOB)
+if not test_result(SETUP_JOB, 'setup-' + JOB):
+    sys.exit(1)
 
 # Split test job.
 if not os.path.exists(WD + '/' + JOB):
     os.makedirs(WD + '/' + JOB)
-SplitJob = [LKPPath + '/sbin/split-job', '--no-default', '--output',
-            WD + '/' + JOB, LKPPath + '/jobs/' + JOB + '.yaml']
-print 'Splitting job %s with command: %s' % (JOB, SplitJob)
-if not test_result(SplitJob, 'split-job-' + JOB):
-    sys.exit(1)
-
-# Setup test job.
-SubTests = glob.glob(WD + '/' + JOB + '/*.yaml')
-print 'Sub-tests of %s: %s' % (JOB, SubTests)
-SetupLocal = [LKPPath + '/bin/setup-local', SubTests[0]]
-print 'Set up %s test with command: %s' % (JOB, SetupLocal)
-if not test_result(SetupLocal, 'setup-local-' + JOB):
+SPLIT_JOB = [LKP_PATH + '/bin/lkp split-job', MONITORS, '--output',
+            WD + '/' + JOB, LKP_PATH + '/jobs/' + JOB + '.yaml']
+print 'Splitting job %s with command: %s' % (JOB, SPLIT_JOB)
+if not test_result(SPLIT_JOB, 'split-job-' + JOB):
     sys.exit(1)
 
 # Delete test results from last lava-test-shell-run.
@@ -100,62 +100,63 @@ if os.path.exists('/result/'):
     shutil.rmtree('/result/', ignore_errors=True)
 
 # Run tests.
-for SubTest in SubTests:
+SUB_TESTS = glob.glob(WD + '/' + JOB + '/*.yaml')
+for SUB_TEST in SUB_TESTS:
     COUNT = 1
     DONE = True
-    SubTestCaseID = os.path.basename(SubTest)[:-5]
-    ResultRoot = str('/'.join(['/result', JOB,
-                               SubTestCaseID[int(len(JOB) + 1):], HostName,
-                               DIST, CONFIG, KernelVersion]))
-    while (COUNT <= LOOPS):
+    SUB_TEST_CASE_ID = os.path.basename(SUB_TEST)[:-5]
+    RESULT_ROOT = str('/'.join(['/result', JOB,
+                               SUB_TEST_CASE_ID[int(len(JOB) + 1):],
+                               HOST_NAME, DIST, CONFIG, KERNEL_VERSION]))
+    while COUNT <= LOOPS:
         # Use suffix for mutiple runs.
         if LOOPS > 1:
             SUFFIX = '-run' + str(COUNT)
         else:
             SUFFIX = ''
 
-        RunLocal = [LKPPath + '/bin/run-local', SubTest]
-        print 'Running test %s%s with command: %s' % (SubTestCaseID, SUFFIX,
-                                                      RunLocal)
-        if not test_result(RunLocal, 'run-local-' + SubTestCaseID + SUFFIX):
+        RUN_COMMAND = [LKP_PATH + '/bin/lkp run', SUB_TEST]
+        print 'Running test %s%s with command: %s' % (SUB_TEST_CASE_ID,
+                                                      SUFFIX, RUN_COMMAND)
+        if not test_result(RUN_COMMAND, 'run-' + SUB_TEST_CASE_ID + SUFFIX):
             DONE = False
             break
 
         # For each run, decode JOB.json to pick up the scores produced by the
         # benchmark itself.
-        ResultFile = ResultRoot + '/' + str(COUNT - 1) + '/' + JOB + '.json'
-        if not os.path.isfile(ResultFile):
-            print '%s not found' % (ResultFile)
+        RESULT_FILE = RESULT_ROOT + '/' + str(COUNT - 1) + '/' + JOB + '.json'
+        if not os.path.isfile(RESULT_FILE):
+            print '%s not found' % (RESULT_FILE)
         else:
-            JsonData = open(ResultFile)
-            DICT = json.load(JsonData)
+            json_data = open(RESULT_FILE)
+            DICT = json.load(json_data)
             for item in DICT:
-                call(['lava-test-case', SubTestCaseID + '-' + item + SUFFIX,
+                call(['lava-test-case', SUB_TEST_CASE_ID + '-' + item + SUFFIX,
                       '--result', 'pass', '--measurement', str(DICT[item][0])])
-            JsonData.close()
+            json_data.close()
 
         COUNT = COUNT + 1
 
     # For mutiple runs, if all runs are completed and results found, decode
     # avg.json.
     if LOOPS > 1 and DONE:
-        AvgFile = ResultRoot + '/' + 'avg.json'
-        if not os.path.isfile(ResultFile):
-            print '%s not found' % (ResultFile)
-        elif not os.path.isfile(AvgFile):
-            print '%s not found' % (AvgFile)
+        AVG_FILE = RESULT_ROOT + '/' + 'avg.json'
+        if not os.path.isfile(RESULT_FILE):
+            print '%s not found' % (RESULT_FILE)
+        elif not os.path.isfile(AVG_FILE):
+            print '%s not found' % (AVG_FILE)
         else:
-            JsonData = open(ResultFile)
-            AvgJsonData = open(AvgFile)
-            DICT = json.load(JsonData)
-            AvgDict = json.load(AvgJsonData)
+            json_data = open(RESULT_FILE)
+            avg_json_data = open(AVG_FILE)
+            DICT = json.load(json_data)
+            AVG_DICT = json.load(avg_json_data)
             for item in DICT:
                 if item in AvgDict:
                     call(['lava-test-case',
-                          SubTestCaseID + '-' + item + '-avg', '--result',
-                          'pass', '--measurement', str(AvgDict[item])])
-            JsonData.close()
-            AvgJsonData.close()
+                          SUB_TEST_CASE_ID + '-' + item + '-avg', '--result',
+                          'pass', '--measurement', str(AVG_DICT[item])])
+            json_data.close()
+            avg_json_data.close()
 
     # Compress and attach raw data.
     call(['tar', 'caf', 'lkp-result-' + JOB + '.tar.xz', '/result/' + JOB])
