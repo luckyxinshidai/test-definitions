dc57f1a9f631d92f4943365496775711c764909f
Chase Qi <chase.qi@linaro.org>
2015-08-06 09:29:23 +0000

    common: fix lkp compare wrapper pep8 issues

--- common/LKP/resultcompare.py
+++ common/LKP/resultcompare.py
@@ -11,15 +11,16 @@ LAVA_JOB1 = sys.argv[1]
 LAVA_JOB2 = sys.argv[2]
 WD = os.getcwd()
 
-def get_result(lava_job):                                                      
-    print('Downloading %s lkp test results...' %lava_job)
-    subprocess.call(['../lavadownload.py', '-n', 'lkp-result',                  
-                             '-l', 'https://validation.linaro.org/RPC2/',               
-                             '-i', lava_job])                                          
-    for result_package in glob.glob('*.tar.xz'):                                        
-        subprocess.check_output(['tar', 'xvf', result_package])                                  
-        os.remove(result_package)                                                       
-    shutil.rmtree('/result', ignore_errors=True)                                
+
+def get_result(lava_job):
+    print('Downloading %s lkp test results...' % lava_job)
+    subprocess.call(['../lavadownload.py', '-n', 'lkp-result',
+                     '-l', 'https://validation.linaro.org/RPC2/',
+                     '-i', lava_job])
+    for result_package in glob.glob('*.tar.xz'):
+        subprocess.check_output(['tar', 'xvf', result_package])
+        os.remove(result_package)
+    shutil.rmtree('/result', ignore_errors=True)
     shutil.copytree('./result', '/result')
 
 
@@ -44,8 +45,8 @@ for commit in os.listdir(commits_directory):
     if commit != commit1:
         commit2 = commit
 
-print('The first commit: %s' %commit1)
-print('The second commit: %s' %commit2)
+print('The first commit: %s' % commit1)
+print('The second commit: %s' % commit2)
 print('Result files to compare:')
 print('\n'.join(results_to_compare))
 print('\n')
@@ -54,7 +55,7 @@ os.chdir('../')
 os.environ['LINUX_GIT'] = '/root/linux-linaro-stable'
 # todo, add linux repo check.
 if not os.path.exists('lkp-tests'):
-    subprocess.check_ouput(['git', 'clone', 
+    subprocess.check_ouput(['git', 'clone',
                             'https://github.com/chase-qi/lkp-tests'])
 
 result_dict = {}
@@ -65,20 +66,20 @@ for result_file in results_to_compare:
     dict = json.load(json_data)
     for item in dict:
         diff = subprocess.check_output(['./lkp-tests/sbin/compare', '-f', item,
-                                         result_directory + '/' + commit1,
-                                         result_directory + '/' + commit2])
+                                        result_directory + '/' + commit1,
+                                        result_directory + '/' + commit2])
         key = subtest_id + '-' + item
-        result_dict[key] = diff.split('\n')[5] + '-' + subtest_id 
+        result_dict[key] = diff.split('\n')[5] + '-' + subtest_id
     json_data.close()
 
 # Print comparison results and save them to txt file.
 output = open(commit1[0:7] + '_' + commit2[0:7] + '_comparison.txt', 'w')
 header = LAVA_JOB1 + '-' + commit1[0:7] + '      %changes  ' + \
-         LAVA_JOB2 + '-' + commit2[0:7] + '  test_measurement-testcase'
+    LAVA_JOB2 + '-' + commit2[0:7] + '  test_measurement-testcase'
 print(header)
 output.write(header + '\n')
 for item in sorted(result_dict):
     print result_dict[item]
     output.write(result_dict[item] + '\n')
 output.close()
-print('Result saved to %s_%s_comparison.txt' %(commit1[0:7], commit2[0:7]))
+print('Result saved to %s_%s_comparison.txt' % (commit1[0:7], commit2[0:7]))
