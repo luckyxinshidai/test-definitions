3aa8644805a4434a9601bd90fa46f4289112e6e8
Chase Qi <chase.qi@linaro.org>
2016-08-25 11:04:43 +0800

    v2: use test-definition as relative root

--- automated/bin/setenv.sh
+++ automated/bin/setenv.sh
@@ -2,5 +2,12 @@
 
 export REPO_PATH="$(pwd)"
 echo "REPO_PATH: ${REPO_PATH}"
-export PATH="${REPO_PATH}/bin":"${PATH}"
+
+if ! [ -d "${REPO_PATH}/automated/bin" ]; then
+    echo "ERROR: Please execute the below command from test-definitions DIR"
+    echo "    . ./automated/bin/setenv.sh"
+    exit 1
+fi
+
+export PATH="${REPO_PATH}/automated/bin":"${PATH}"
 echo "BIN_PATH: ${PATH}"
