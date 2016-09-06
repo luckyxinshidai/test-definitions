3f3d3520ef93e338340849cc23140680ed82d961
Chase Qi <chase.qi@linaro.org>
2016-09-06 16:03:29 +0800

    v2: lamp: added support for ubuntu
    Signed-off-by: Chase Qi <chase.qi@linaro.org>

--- automated/linux/lamp/lamp.sh
+++ automated/linux/lamp/lamp.sh
@@ -17,6 +17,13 @@ case "${dist}" in
     systemctl start apache2
     systemctl start mysql
     ;;
+  Ubuntu)
+    title="Apache2 Ubuntu Default Page: It works"
+    pkgs="curl apache2 mysql-server php-mysql php-common libapache2-mod-php"
+    install_deps "${pkgs}"
+    systemctl start apache2
+    systemctl start mysql
+    ;;
   CentOS)
     title="Apache HTTP Server Test Page powered by CentOS"
     pkgs="curl httpd mariadb-server mariadb php php-mysql"
@@ -25,11 +32,12 @@ case "${dist}" in
     systemctl start mariadb
     ;;
   *)
-    error_info "Unsupported distribution"
+    error_msg "Unsupported distribution!"
 esac
 
 # Test Apache.
-curl http://localhost | grep "${title}"
+curl -o "${OUPUT}"/index.html http://localhost
+grep "${title}" "${OUPUT}"/index.html
 check_return "apache2"
 
 # Test mysql.
@@ -38,5 +46,6 @@ check_return "mysql"
 
 # Test php.
 echo "<?php phpinfo(); ?>" > /var/www/html/info.php
-curl http://localhost/info.php | grep "PHP Version"
+curl -o "${OUTPUT}"/phpinfo.html http://localhost/info.php
+grep "PHP Version" "${OUTPUT}"/phpinfo.html
 check_return "php"
