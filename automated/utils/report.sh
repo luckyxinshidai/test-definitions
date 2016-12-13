21a7910ad4ad12654326e79960cfde6b7e8755f8
Chase Qi <chase.qi@linaro.org>
2016-12-13 10:38:48 +0800

    added report.sh for result collecting
    Change-Id: I95fddd9d84bfb33e0331093209f77ea6d71ad642
    Signed-off-by: Chase Qi <chase.qi@linaro.org>

---
+++ automated/utils/report.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -ex
+
+# shellcheck disable=SC1091
+. ../lib/sh-test-lib
+DIR=$1
+OUTPUT=$2
+
+mkdir -p "${OUTPUT}"
+for plan in func enterprise perf ltp libhugetlbfs; do
+    if [ -f "${DIR}/${plan}/result.csv" ]; then
+        cp "${DIR}/${plan}/result.csv" "${OUTPUT}/${plan}.csv"
+    fi
+
+    if [ -f "${DIR}/${plan}/result.json" ]; then
+        cp "${DIR}/${plan}/result.json" "${OUTPUT}/${plan}.json"
+    fi
+done
+
+mkdir -p "${OUTPUT}/context"
+# kernel version
+uname -a > "${OUTPUT}/context/kernel-version"
+
+# firmware version
+command -v dmidecode || install_deps "dmidecode"
+dmidecode -t bios > "${OUTPUT}/context/bios-version"
+
+# installed packages
+dist_name
+# shellcheck disable=SC2154
+case "${dist}" in
+    Debian|Ubuntu) dpkg-query -l > "${OUTPUT}/context/installed-pkgs" ;;
+    CentOS|Fedora) rpm -qa > "${OUTPUT}/context/installed-pkgs" ;;
+    *) error_msg "Unsupported distro: ${dist}" ;;
+esac
